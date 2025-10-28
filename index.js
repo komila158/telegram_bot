6f0f36779f8d3854eef45ec281b3efa301779ff4
komila <komilovakomila1104@gmail.co>
2025-10-28 16:34:53 +0500

    scghyx bn

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const TOKEN = "8483602290:AAEl3VIqFBH0XxcBQEQnLGCkSNhBJb7K5Ws";
 const bot = new TelegramBot(TOKEN, { polling: true});
 
 bot.on("message" , function (msg) {
+
     const chatId = msg.chat.id;
     const text = msg.text;
+    const firstname = msg.chat.first_name;
 
-    bot.sendMessage(chatId, `Salom -> ${text}`);
+    bot.sendMessage(chatId, `hush kelipsiz, ${firstname}`,{
+     reply_markup: {
+        keyboard: [
+            [{text: "Boshlash🧠"}],
+            [{text: "Menu 🫠"}, {text: "Sozlamalar🫆"}],
+        ],
+        resize_keyboard: true,
+     }
+    });
+
 
     console.log(msg);
     
