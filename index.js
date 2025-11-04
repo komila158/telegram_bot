bdeb71aa7070870c323f5bcf90a527e982070ce7
komila <komilovakomila1104@gmail.co>
2025-11-04 16:32:23 +0500

    done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on("message", async function (msg) {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstname = msg.chat.first_name;
-  const menuPhoto = "./assets/images/1.webp";
+  const menuPhoto = "./rasm.avif";
 
   if (text == "/start") {
     bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`, {
@@ -21,11 +21,27 @@ bot.on("message", async function (msg) {
       },
     });
   } else if (text == "Boshlash 🔥") {
-    bot.sendMessage(chatId, "Salom, sizga qanday yordam bera olaman?");
+    // bot.sendMessage(chatId, "Salom, sizga qanday yordam bera olaman?");
+
+    bot.sendPhoto(chatId, "./m5.jpg", {
+      caption: `
+      The BMW M5 is a high-performance luxury sedan that blends a business sedan's comfort with a sports car's power, currently featuring a plug-in hybrid system with a twin-turbo V8 engine and electric motor.
+      `,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "Info", callback_data: "info" },
+            { text: "price", callback_data: "price" },
+          ],
+          [{ text: "photos", callback_data: "photos" },],
+        ],
+
+      },
+    })
   } else if (text == "Menu 😜") {
     const kutingXabari = await bot.sendMessage(chatId, "Iltimos kuting...");
 
-  bot.sendMessage(chatId, `Salom -> ${text}`);
+    bot.sendMessage(chatId, `Salom -> ${text}`);
     setTimeout(function () {
       bot.deleteMessage(chatId, kutingXabari.message_id);
       bot.sendPhoto(chatId, menuPhoto, {
@@ -40,6 +56,25 @@ bot.on("message", async function (msg) {
     }, 1000);
   }
 
+  bot.on("callback_query", function (query) {
+  console.log(query);
+  const data = query.data;
+  const chatId = query.message.chat.id;
+
+  if (data == "info") {
+    bot.sendMessage(chatId, "Bu yerda Lamborghini haqida ma'lumot olasiz");
+  } else if (data == "photos") {
+    bot.sendPhoto(chatId, "./m5.jpg");
+  } else if (data == "price") {
+    bot.sendMessage(chatId, "175,000 dollar", {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Sotib olish", callback_data: "buy" }]],
+      },
+    });
+  } else if (data == "buy") {
+    bot.sendMessage(chatId, "Pullarni Soliyajonga bering... Mashina unda");
+  }
+});
   console.log(msg);
   //   console.log("*********");
   //   console.log(chatId, text);
